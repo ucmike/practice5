d447dc6ad48349d1356cd8f31a01668634466143
ucmike <seitzme@mail.uc.edu>
2021-02-17 00:43:15 +0000

    added a couple of my comments

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ import java.util.Random;
 
 class Main {
   public static void main(String[] args) {
-    int a =10;
+    int a =14;
     int b =13;
     int c =18;
-    findMax(a, b);
-    System.out.println("Should be 7: " + findMax2(a, b));
+    findMax(a, b); //changed to variables
+    System.out.println("Should be 14: " + findMax2(a, b));
     System.out.println();
     
-    System.out.println("Should be 9: " + findMax3(a, b, c));
+    System.out.println("Should be 18: " + findMax3(a, b, c));
     System.out.println("Should be 9: " + findMax3(c, a, b));
     System.out.println("Should be 9: " + findMax3(b, c, a));
     System.out.println("Should be 9: " + findMax3(a, b, c));
@@ -19,8 +19,8 @@ class Main {
     System.out.println();
     
     System.out.println("min is: " + findMinArray2D());
-    System.out.println();    // just for formatting
-    int numsArr[] = new int[7];
+    System.out.println();   
+    int numsArr[] = new int[10];
     Random r = new Random();
     for (int i = 0; i < numsArr.length; i++) {
       numsArr[i] = r.nextInt(51);
@@ -28,21 +28,21 @@ class Main {
     System.out.println("min is: " + findMinArray1D(numsArr));
   }
 
-  // practice problem 1
-  static void findMax(int x, int y) {
+  
+  static void findMax(int x, int y) { //notice this doesn't return but rather prints the result
           if(x >= y)System.out.println(x);
           else System.out.println(y);
           }
 
-  // practice problem 2
-  static int findMax2(int x, int y) {
+ 
+  static int findMax2(int x, int y) { // notice that this option has a return that we have to call from main.
     if (x >= y)
       return x;
     else
       return y;
   }
 
-  // practice problem 3
+
   static int findMax3(int x, int y, int z) {
     if (x >= y && x >= z)
       return x;
@@ -52,7 +52,7 @@ class Main {
       return z;
   }
 
-  // practice problem 4
+ 
   static int findMinArray() {
     int nums[] = new int[7];
     Random r = new Random();
@@ -71,7 +71,6 @@ class Main {
     return min;
   }
 
-  // practice problem 5
   static int findMinArray2D() {
     int nums[][] = new int[3][7];
     Random r = new Random();
@@ -90,14 +89,14 @@ class Main {
     }
     for (int i = 0; i < nums.length; i++) {
       for (int j = 0; j < nums[0].length; j++) {
-        System.out.print(nums[i][j] + " ");
+        System.out.print(nums[i][j] + " ");//puts a space after each number
       }
       System.out.println();
     }
     return min;
   }
 
-  // practice problem 6
+
   static int findMinArray1D(int nums[]) {
     int min = nums[0];
     for (int i = 0; i < nums.length; i++) {
